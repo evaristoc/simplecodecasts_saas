3dccabce26ffed1ceca6531c19fa987b2a76d379
ec <varikvi@yahoo.com>
2015-06-07 14:17:54 +0200

    Added mailer: class (mailer folder), controller and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,10 @@ class ContactsController < ApplicationController
   def create # this is coming from the post action !
     @contact = Contact.new(contact_params)
     if @contact.save #committing into the database
+      name = params[:contact][:name] # from here, getting the data from the db to be sent by the email below...
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      ContactMailer.contact_mail(name, email, body).deliver # ... sending data to our email
       flash[:success] = "Message sent."
       redirect_to root_path
       #redirect_to new_contact_path, notice: "Message sent." # OJO: redirection is into contact_new...
