fb5aeea5f53b700eca1d9b0176ca2973cb6aa97c
ec <varikvi@yahoo.com>
2015-06-07 13:50:24 +0200

    Added redirections, validations and flash messages with styling

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,24 @@
 class ContactsController < ApplicationController
-  def new
+  def new # this is the form called after the get action redirection from the index ! 
     @contact = Contact.new
   end
   
-  def create
+  def create # this is coming from the post action !
+    @contact = Contact.new(contact_params)
+    if @contact.save #committing into the database
+      flash[:success] = "Message sent."
+      redirect_to root_path
+      #redirect_to new_contact_path, notice: "Message sent." # OJO: redirection is into contact_new...
+    else
+      flash[:danger] = "Error ocurred, message not sent."
+      redirect_to new_contact_path
+      #redirect_to new_contact_path, notice: "Error occured."
+    end
+    
   end
+  
+  private # a new, important addition to rails ! security !!
+    def contact_params # "white-listing" parameters...
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
